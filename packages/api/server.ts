c0eb8fe8fe72ee357c66dd6213126d0c68b2a63d
Bart <bart@maroon-studio.com>
2024-05-27 09:42:21 +0200

    fix: fix lack of comments in server.ts file

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -3,13 +3,16 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
+// Configs
 const app: Express = express()
 const port = process.env.PORT || 3000
 
+// Creating main route section
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + Typescript Server")
 })
 
+// Server listening section
 app.listen(port, () => {
     console.log(`> running on ${port} port`)
 })
